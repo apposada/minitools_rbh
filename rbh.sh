8ff10a46fa6dd4353d5de1588d1ae4fc8a56373c
Alberto Perez Posada <alperez@login1.hpc.crg.es>
2026-03-04 16:01:31 +0100

    changed 24 for 12 threads

--- rbh.sh
+++ rbh.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 blastp -db $1 -query $2 \
-                -max_target_seqs 1 -evalue 10E-5 -num_threads 24 -outfmt "6 qseqid sseqid qcovhsp evalue" | \
+                -max_target_seqs 1 -evalue 10E-5 -num_threads 12 -outfmt "6 qseqid sseqid qcovhsp evalue" | \
                 tee rbh_1_2_orig.tsv | \
                 cut -f1,2 > rbh_1_2.tsv && \
                 blastp -db $2 -query $1 \
-                -max_target_seqs 1 -evalue 10E-5 -num_threads 24 -outfmt "6 sseqid qseqid qcovhsp evalue" | \
+                -max_target_seqs 1 -evalue 10E-5 -num_threads 12 -outfmt "6 sseqid qseqid qcovhsp evalue" | \
                 tee rbh_2_1_orig.tsv | \
                 cut -f1,2 > rbh_2_1.tsv && \
                 python3 common_lines.py rbh_2_1.tsv rbh_1_2.tsv final_RBH.tsv
