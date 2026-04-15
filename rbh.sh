2f9a1eb68a719f337c5b544739ad741927b75107
apposada <ap.posada1@gmail.com>
2026-04-15 11:11:55 +0200

    handle paths, input/output file names

--- rbh.sh
+++ rbh.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
-blastp -db $1 -query $2 \
+
+# variables
+fa1=$1
+fa2=$2
+# retrieve path for outputs
+outpath=$(dirname $fa1)
+# retrieve base name without file extensions for sp1
+basename1=$(basename $fa1)
+sp1=${basename1//.*}
+# retrieve base name without file extensions for sp2
+basename2=$(basename $fa2)
+sp2=${basename2//.*}   
+# define output file name prefix
+outfn="${outpath}/${sp1}__v__${sp2}"
+
+set -e
+set --
+
+# blastp and common_lines.py
+blastp -db $fa1 -query $fa2 \
                 -max_target_seqs 1 -evalue 10E-5 -num_threads 12 -outfmt "6 qseqid sseqid qcovhsp evalue" | \
-                tee rbh_1_2_orig.tsv | \
-                cut -f1,2 > rbh_1_2.tsv && \
-                blastp -db $2 -query $1 \
+                tee ${outfn}_rbh_1_2_orig.tsv | \
+                cut -f1,2 > ${outfn}_rbh_1_2.tsv && \
+                blastp -db $fa2 -query $fa1 \
                 -max_target_seqs 1 -evalue 10E-5 -num_threads 12 -outfmt "6 sseqid qseqid qcovhsp evalue" | \
-                tee rbh_2_1_orig.tsv | \
-                cut -f1,2 > rbh_2_1.tsv && \
-                python3 common_lines.py rbh_2_1.tsv rbh_1_2.tsv final_RBH.tsv
+                tee ${outfn}_rbh_2_1_orig.tsv | \
+                cut -f1,2 > ${outfn}_rbh_2_1.tsv && \
+                common_lines.py ${outfn}_rbh_2_1.tsv ${outfn}_rbh_1_2.tsv ${outfn}_final_RBH.tsv
